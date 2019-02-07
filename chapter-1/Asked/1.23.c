53b13546e2bc124771944baff795cd7f347910dd
Rajneesh Shukla <44999300+rajneesh18@users.noreply.github.com>
2019-02-07 12:32:26 +0530

    Update remove comments program
    In this update, we manage the quoted strings

--- chapter-1/Asked/1.23.c
+++ chapter-1/Asked/1.23.c
@@ -12,6 +12,7 @@ int main(){
     while((c = getchar()) != EOF){
         rcomment(c);
     }
+    return 0;
 }
 
 void rcomment(int c){
@@ -19,7 +20,7 @@ void rcomment(int c){
     if(c == '/'){
         if((d = getchar()) == '*'){
             incomment();
-        }else if(d == '/'){
+        }else if(d == '/'){               /* This block is for removing single line comments */
             x:
             if(d == '\n' || d == '\0' || d == EOF){
                 putchar('\n');
@@ -31,15 +32,19 @@ void rcomment(int c){
             putchar(c);
             putchar(d);
         }
-    }else if(c == '"' || c == '\''){
+    }else if(c == '"' || c == '\''){        /* This will handle the quoted strings and character constants properly. */
         putchar(c);
         while((d = getchar()) != c){
             putchar(d);
+            if(d == '\\'){
+                putchar(getchar());
+            }
         }
     }else{
         putchar(c);
     }
 }
+/* incomment: to remove multiple line comments */
 void incomment(){
     int c,d;
     c = getchar();
